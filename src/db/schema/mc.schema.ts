6c1909ad462bb410f627d7c7d2ea87b0766a24ca
mtopn <mengting@opn.ooo>
2025-03-12 12:45:51 +0700

    chore: mc schema

---
+++ src/db/schema/mc.schema.ts
@@ -0,0 +1,23 @@
+import { pgTable as table } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
+
+import { PREFIX } from "@/constants";
+import { brandedUid, timestamps } from "../helpers/columns.helpers";
+import { user } from "./user.schema";
+
+export const mc = table(
+  "mc",
+  {
+    id: t.serial("id").primaryKey(),
+    uid: brandedUid(PREFIX.MC),
+    name: t.varchar("name", { length: 255 }),
+    userId: t
+      .integer("user_id")
+      .references(() => user.id, { onDelete: "cascade" }),
+    ...timestamps,
+  },
+  (table) => [
+    t.uniqueIndex("uid_idx").on(table.uid),
+    t.index("name_idx").on(table.name),
+  ]
+);
