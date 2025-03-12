1ec807659d6323051d5020f3675a0fb582a00564
mtopn <mengting@opn.ooo>
2025-03-12 12:45:05 +0700

    chore: city schema

---
+++ src/db/schema/city.schema.ts
@@ -0,0 +1,19 @@
+import { pgTable as table } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
+
+import { PREFIX } from "@/constants";
+import { brandedUid, timestamps } from "../helpers/columns.helpers";
+
+export const city = table(
+  "city",
+  {
+    id: t.serial("id").primaryKey(),
+    uid: brandedUid(PREFIX.CITY),
+    name: t.varchar("name", { length: 255 }).unique().notNull(),
+    ...timestamps,
+  },
+  (table) => [
+    t.uniqueIndex("uid_idx").on(table.uid),
+    t.uniqueIndex("name_idx").on(table.name),
+  ]
+);
