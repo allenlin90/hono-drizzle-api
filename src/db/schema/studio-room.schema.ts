7c6c7d642d3e05f95e95d1fcd0d61b817eb6d893
mtopn <mengting@opn.ooo>
2025-03-12 12:45:38 +0700

    chore: studio-room schema

---
+++ src/db/schema/studio-room.schema.ts
@@ -0,0 +1,27 @@
+import { pgTable as table } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
+
+import { PREFIX } from "@/constants";
+import { brandedUid, timestamps } from "../helpers/columns.helpers";
+import { studio } from "./studio.schema";
+
+export const roomTypeEnum = t.pgEnum("studio_room_type", ["s", "m", "l"]);
+
+export const studioRoom = table(
+  "studio_room",
+  {
+    id: t.serial("id").primaryKey(),
+    uid: brandedUid(PREFIX.STUDIO_ROOM),
+    name: t.varchar("name", { length: 255 }).notNull(),
+    studioId: t
+      .integer("studio_id")
+      .references(() => studio.id, { onDelete: "cascade" })
+      .notNull(),
+    type: roomTypeEnum().default("s").notNull(),
+    ...timestamps,
+  },
+  (table) => [
+    t.uniqueIndex("uid_idx").on(table.uid),
+    t.uniqueIndex("name_idx").on(table.name),
+  ]
+);
