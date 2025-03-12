207fda82133b9fdf65b80b5a4737022e689741b0
mtopn <mengting@opn.ooo>
2025-03-12 13:23:29 +0700

    chore: schema re-export

---
+++ src/db/schema/index.ts
@@ -0,0 +1,7 @@
+export * from "./relations";
+export { address } from "./address.schema";
+export { city } from "./city.schema";
+export { mc } from "./mc.schema";
+export { studio } from "./studio.schema";
+export { studioRoom, roomTypeEnum } from "./studio-room.schema";
+export { user } from "./user.schema";
