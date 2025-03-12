2c36f87f3e2c88edacaf3e92922860c0262e0888
mtopn <mengting@opn.ooo>
2025-03-12 12:45:15 +0700

    chore: address schema

---
+++ src/db/schema/address.schema.ts
@@ -0,0 +1,26 @@
+import { pgTable as table } from "drizzle-orm/pg-core";
+import * as t from "drizzle-orm/pg-core";
+
+import { PREFIX } from "@/constants";
+import { brandedUid, timestamps } from "../helpers/columns.helpers";
+
+import { city } from "./city.schema";
+
+export const address = table(
+  "address",
+  {
+    id: t.serial("id").primaryKey(),
+    uid: brandedUid(PREFIX.ADDRESS),
+    address: t.varchar("address", { length: 255 }).notNull(),
+    subDistrict: t.varchar("sub_district", { length: 255 }),
+    district: t.varchar("district", { length: 255 }),
+    cityId: t
+      .integer("city_id")
+      .references(() => city.id, { onDelete: "set null" })
+      .notNull(),
+    province: t.varchar("province", { length: 255 }).notNull(),
+    postcode: t.varchar("postcode", { length: 16 }).notNull(),
+    ...timestamps,
+  },
+  (table) => [t.uniqueIndex("uid_idx").on(table.uid)]
+);
