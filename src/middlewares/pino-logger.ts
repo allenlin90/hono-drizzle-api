d25f5a31528c30b537303f9b96550a040bfcaead
mtopn <mengting@opn.ooo>
2025-03-15 21:05:41 +0700

    chore: toggle pino pretty in production

--- src/middlewares/pino-logger.ts
+++ src/middlewares/pino-logger.ts
@@ -1,9 +1,12 @@
 import pino from "pino";
 import { pinoLogger as logger } from "hono-pino";
 import pretty from "pino-pretty";
+import env from "@/env";
 
 export const pinoLogger = () => {
+  const options = env.NODE_ENV === "production" ? {} : pretty();
+
   return logger({
-    pino: pino(pretty()),
+    pino: pino(options),
   });
 };
