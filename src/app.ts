c317a7ed137c1c55e833d0649a74b929bf075f10
mtopn <mengting@opn.ooo>
2025-03-15 20:24:41 +0700

    chore: use logger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
 import notFound from "./middlewares/not-found";
 import onError from "./middlewares/on-error";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+
+app.use(logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
