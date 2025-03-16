760f0c28e0137fa43494e11f209b3bf84022e115
mtopn <mengting@opn.ooo>
2025-03-16 22:24:27 +0700

    chore: query data from db

--- src/routes/brands/brands.handlers.ts
+++ src/routes/brands/brands.handlers.ts
@@ -1,15 +1,9 @@
 import type { AppRouteHandler } from "@/lib/types";
 import type { ListRoute } from "./brands.routes";
+import db from "@/db";
 
-export const list: AppRouteHandler<ListRoute> = (c) => {
-  // TODO: query from db
-  return c.json([
-    {
-      id: 1,
-      uid: "brand-1",
-      name: "Brand 1",
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
-    },
-  ]);
+export const list: AppRouteHandler<ListRoute> = async (c) => {
+  const brands = await db.query.brand.findMany();
+
+  return c.json(brands);
 };
