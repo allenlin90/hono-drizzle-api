de969f816b23c49c560eb3d08a3ed25fbba1b2bd
mtopn <mengting@opn.ooo>
2025-03-19 11:08:04 +0700

    chore: align naming pattern

--- src/openapi/schemas/not-found.ts
+++ src/openapi/schemas/not-found.ts
@@ -1,8 +1,8 @@
 import * as HttpStatusPhrases from "@/http-status-phrases";
 import createMessageObjectSchema from "./create-message-object";
 
-export const notFoundSchema = createMessageObjectSchema(
+export const NotFoundSchema = createMessageObjectSchema(
   HttpStatusPhrases.NOT_FOUND
 );
 
-export default notFoundSchema;
+export default NotFoundSchema;
