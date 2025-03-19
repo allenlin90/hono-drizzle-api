c4a482df04e9e2a701fe5b12db3593ab270ba3d0
mtopn <mengting@opn.ooo>
2025-03-20 00:42:42 +0700

    chore: note TODO

--- src/validators/idempotency-key.ts
+++ src/validators/idempotency-key.ts
@@ -1,13 +1,20 @@
+import { z } from "@hono/zod-openapi";
 import { validator } from "hono/validator";
 import { BAD_REQUEST } from "@/http-status-codes";
 
+const validateKey = (key: string) => z.string().uuid().safeParse(key);
+
+// TODO: store and validate idempotency key
 export const idempotencyKey = validator("header", (value, c) => {
   const idempotencyKey = value["idempotency-key"];
+  const { success } = validateKey(idempotencyKey);
 
-  if (idempotencyKey == undefined || idempotencyKey === "") {
+  if (idempotencyKey == undefined || idempotencyKey === "" || !success) {
     return c.json(
       {
-        message: "Idempotency-Key is required",
+        message: idempotencyKey
+          ? "Invalid Idempotency-Key"
+          : "Idempotency-Key is required",
       },
       BAD_REQUEST
     );
