ea3de59d79d8f2fc0703b6ae857ae7b3d2147d03
mtopn <mengting@opn.ooo>
2025-03-22 22:52:33 +0700

    fix: can query with user_uid

--- src/openapi/schemas/users/user-param-filter.ts
+++ src/openapi/schemas/users/user-param-filter.ts
@@ -1,9 +1,11 @@
 import { NameParams } from "../name-params";
 import { PageParams } from "../page-params";
 import { EmailParams } from "../email-params";
+import { userIdSchema } from "../id-query-params";
 
 export const UserParamFilters = PageParams()
   .merge(NameParams(["username"]))
+  .merge(userIdSchema)
   .merge(EmailParams);
 
 export default UserParamFilters;
