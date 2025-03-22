a5d6e3f1fe958f92e0bf34eed4471ea3bc276b01
mtopn <mengting@opn.ooo>
2025-03-22 12:05:20 +0700

    chore: cleanup

--- src/openapi/schemas/brand-materials/brand-materials-filters.ts
+++ src/openapi/schemas/brand-materials/brand-materials-filters.ts
@@ -1,23 +1,20 @@
 import { z } from "@hono/zod-openapi";
 import { NameParams } from "@/openapi/schemas/name-params";
-import BrandIdQueryParams from "../brand-id-query-params";
 import { brandMaterialTypeEnum } from "@/db/schema/brand-material.schema";
+import BrandIdQueryParams from "../brand-id-query-params";
 import coerceBoolean from "../coerce-boolean";
 
 export const MaterialParamFilters = z
-  .object({})
+  .object({
+    type: brandMaterialTypeEnum,
+    is_active: coerceBoolean.optional().openapi({
+      default: true,
+      examples: [true, false, "1", "0"],
+      description: "Filter activeness of the object",
+    }),
+  })
   .merge(BrandIdQueryParams())
-  .merge(NameParams(["script", "scene"]))
-  .merge(
-    z.object({
-      type: brandMaterialTypeEnum,
-      is_active: coerceBoolean.optional().openapi({
-        default: true,
-        examples: [true, false, "1", "0"],
-        description: "Filter activeness of the object",
-      }),
-    })
-  );
+  .merge(NameParams(["script", "scene"]));
 
 export type MaterialParamFiltersSchema = z.infer<typeof MaterialParamFilters>;
 
