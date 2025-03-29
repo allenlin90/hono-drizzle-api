885bd9b09cd8d57485ded81f294e99ff9c68f793
mtopn <mengting@opn.ooo>
2025-03-29 22:19:28 +0700

    chore: show-platform validator

---
+++ src/openapi/schemas/helpers/show-platform-validator.ts
@@ -0,0 +1,33 @@
+import { z } from "@hono/zod-openapi";
+import { PREFIX } from "@/constants";
+import { idValidators, uidValidator } from "./uid-validators";
+
+export const showPlatformValidator = <T extends z.AnyZodObject>(schema: T) => {
+  return z
+    .object({
+      show_platform_uid: z.string().startsWith(PREFIX.SHOW_PLATFORM),
+    })
+    .merge(schema)
+    .transform(async ({ show_platform_uid, ...values }, ctx) => {
+      const { queryObject } = idValidators["show_platform"];
+      const [showPlatform] = await queryObject(show_platform_uid);
+      const validationResults = await uidValidator(values, ctx);
+
+      if (!showPlatform) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Show-platform not found",
+        });
+
+        return z.NEVER;
+      }
+
+      const { show_id, show_uid, platform_id, platform_uid } = showPlatform;
+
+      return {
+        ...validationResults,
+        show: { id: show_id, uid: show_uid },
+        platform: { id: platform_id, uid: platform_uid },
+      };
+    });
+};
